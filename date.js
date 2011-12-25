870ab8fa91b72eb85d3335a52cd953d53cce8322
Eric Miotto <edymtt@netscape.net>
2011-12-25 16:40:14 +0100

    first commit

---
+++ date.js
@@ -0,0 +1,11 @@
+function displayDate()
+{
+document.getElementById("demo").innerHTML=Date();
+}
+
+function printSomeFluff()
+{
+	document.write("<h1>This is a heading</h1>");
+document.write("<p>This is a paragraph.</p>");
+document.write("<p>This is another paragraph</p>");
+}
